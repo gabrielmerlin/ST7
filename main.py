7f0c84bd9f92dd6a8d899bb4bdf3f8f9552fbd63
merlin <gabriel.merlin@student-cs.fr>
2020-02-26 18:44:33 +0100

    Dico selon les sédols pour market

--- main.py
+++ main.py
@@ -3,17 +3,15 @@
 import json
 import yfinance as yf
 
+# Chargement des données
+
 data_file = open("data_ST7MDS.json")
 data = json.load(data_file)
-i=1
+market = {}
 for value in data['Mapping']:
     price = yf.Ticker(value['Ticker'])
-    price_history = price.history(period="max")
-    if i==1:
-        market = price_history
-        market["Sedol"] = value["Sedol"]
-        print(market)
-    else:
-        price_history["Sedol"]=value["Sedol"]
-        market=market.merge(price_history)
-    i+=1
+    market[value["Sedol"]] = price.history(period="max")
+
+# S&P 250
+
+#for t in range()
