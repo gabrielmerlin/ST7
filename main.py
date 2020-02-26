0c6709ec6496c860e2e024b47227c92c4c9eefc1
merlin <gabriel.merlin@student-cs.fr>
2020-02-26 18:24:40 +0100

    Ajout d'une colonne à market contenant les sedols.

--- main.py
+++ main.py
@@ -7,10 +7,13 @@ data_file = open("data_ST7MDS.json")
 data = json.load(data_file)
 i=1
 for value in data['Mapping']:
-    price= yf.Ticker(value['Ticker'])
+    price = yf.Ticker(value['Ticker'])
+    price_history = price.history(period="max")
     if i==1:
-        market=price.history(period= "max")
+        market = price_history
+        market["Sedol"] = value["Sedol"]
+        print(market)
     else:
-        market=market.merge(price.history(period="max"))
+        price_history["Sedol"]=value["Sedol"]
+        market=market.merge(price_history)
     i+=1
-
