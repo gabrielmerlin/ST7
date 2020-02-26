f7ad029de6ab0fbeb00dc7fdf0f68b097bb6209c
djomandanho <45032372+djomandanho@users.noreply.github.com>
2020-02-26 17:56:35 +0100

    Update main.py
    création de la table market à partir de yfinance

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ import yfinance as yf
 
 data_file = open("data_ST7MDS.json")
 data = json.load(data_file)
+print (data)
+i=1
+for value in data['Mapping']:
+    price= yf.Ticker(value['Ticker'])
+    if i==1:
+        market=price.history(max)
+    else:
+        market=market.merge(price.history(max))
+    i+=1
