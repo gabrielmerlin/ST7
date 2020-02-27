07173897abb76387205ae5c012bc6257445b7592
merlin <gabriel.merlin@student-cs.fr>
2020-02-27 12:32:30 +0100

    Changement des bornes pour l'import avec yfinance.

--- main.py
+++ main.py
@@ -3,14 +3,17 @@
 import json
 import yfinance as yf
 
-# Chargement des données
+# Chargement des données json
 
 data_file = open("data_ST7MDS.json")
 data = json.load(data_file)
 market = {}
+
+# Chargement des données yfinance
 for value in data['Mapping']:
     price = yf.Ticker(value['Ticker'])
-    market[value["Sedol"]] = price.history(period="max")
+    market[value["Sedol"]] = price.history(start="2002-12-31", end="2020-02-14")
+
 
 # S&P 250
 
