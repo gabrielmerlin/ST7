def5d1a1fb461d37df9b08473437b0a51661e849
merlin <gabriel.merlin@student-cs.fr>
2020-02-27 17:48:10 +0100

    Réorganisation des données.

--- main.py
+++ main.py
@@ -2,18 +2,46 @@
 
 import json
 import yfinance as yf
+import pandas as pd
+import date_formatting
 
 # Chargement des données json
 
 data_file = open("data_ST7MDS.json")
 data = json.load(data_file)
-market = {}
+
+unordonned_market_cap_evol = data['MarketCap']
+
+dic = {}
+
+for i in range(len(unordonned_market_cap_evol)):
+    date_i = date_formatting.date_formate(unordonned_market_cap_evol[i]['Date'][0])
+
+    unordonned_market_caps = unordonned_market_cap_evol[i]['MarketCap']
+
+    market_caps = {}
+
+    for j in range(len(unordonned_market_caps)):
+        sedol, market_cap = unordonned_market_caps[j]['Sedol'], unordonned_market_caps[j].get('MarketCap', 0)
+        market_caps[sedol] = market_cap
+
+    dic[date_i] = market_caps
+
+market_caps = pd.DataFrame(dic).T   # Cette table (ligne: date, colonne: sedol) panda contient les marketcaps
 
 # Chargement des données yfinance
+market_list = {}
+sedol_list = []
+i = 0
 for value in data['Mapping']:
-    price = yf.Ticker(value['Ticker'])
-    market[value["Sedol"]] = price.history(start="2002-12-31", end="2020-02-14")
-
+    if i < 2:
+        price = yf.Ticker(value['Ticker'])
+        market_list[value["Sedol"]] = price.history(start="2002-12-31", end="2020-02-14")
+        #print(type(market_dic[value["Sedol"]]))
+        sedol_list.append(value["Sedol"])
+        i += 1
+market = pd.concat(market_list, keys=sedol_list).sort_index()
+print(market)   # Cette table panda possède un multi-indice (sedol, date)
 
 # S&P 250
 
