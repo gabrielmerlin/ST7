993a936d5af5090d027fc8b4c7f7392a182a2060
merlin <gabriel.merlin@student-cs.fr>
2020-03-09 14:45:42 +0100

    Séparation du calcul de mu et de sigma de celui de l'optimisation.

--- Optimisateur_donnéeshistoriques_2ans.py
+++ Optimisateur_donnéeshistoriques_2ans.py
@@ -3,11 +3,55 @@ import cvxpy as cp
 import pandas as pd
 import json
 import datetime
+
 data_file = open("data_ST7MDS.json")
 data = json.load(data_file)
 market_cap = data['MarketCap']
 date_debut= datetime.datetime(2005,1,1)
 print(date_debut)
+
+def mean_covariance_matrix_over_time(market):
+    """
+    Cette fonction estime pour chaque date mu et sigma
+
+    :param market: Dataframe Panda contenant les données de marché
+    :return: Dictionnaire associant à chaque date le couple mu, sigma
+    """
+    rendements = (market['Close'] - market['Close'].shift()) / (market['Close'].shift())
+    rendements = rendements.unstack(0).sort_index()
+
+    date_debut = datetime.datetime(2005, 1, 1)
+    print(date_debut)
+    date_fin = datetime.datetime(2020, 2, 1)
+
+    mu_sigma_dic = {}
+
+    while date_debut <= date_fin:
+        # On parcourt chaque début de mois pour trouver une stratégie correspondante
+        yeard = date_debut.year-2
+        monthd = date_debut.month
+        dayd = date_debut.day
+
+        #determination des bornes des données historiques
+        debut = datetime.datetime(yeard, monthd, dayd)
+        fin = date_debut - datetime.timedelta(days=1)
+
+        rendements_periode = rendements.loc[slice(debut, fin)]
+
+        # calcul du vecteur des rendements à l'aide des données historiques
+        mu = rendements_periode.mean()
+
+        #création de la matrice de covariance à l'aide des données historiques
+        sigma = rendements_periode.cov()
+
+        mu_sigma_dic[date_debut] = mu, sigma
+
+        if monthd<12:
+            date_debut=datetime.datetime(yeard, monthd+1, dayd)
+        else:
+            date_debut=datetime.datetime(yeard+1, 1, 1)
+
+    return mu_sigma_dic
 
 def optimasateur(market,data,sedol):
     n=len (data)
